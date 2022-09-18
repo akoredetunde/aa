4ef119d3a6ec36db595181e691056e700041efe0
Olatunde Amos <98148042+akoredetunde@users.noreply.github.com>
2022-09-18 15:25:50 +0100

    aa

--- index.js
+++ index.js
@@ -1,36 +1,23 @@
-// const countDownDate = new Date("Nov 29, 2022 12:11:12").getTime();
-// let x = setInterval(function () {
-//   let now = new Date().getTime();
-//   let distance = countDownDate - now;
-//   let days = Math.floor(distance / (1000*60*60*24));
-//   let hours = Math.floor((distance % (1000*60*60*24)) / (1000*60*60));
-//   let minutes = Math.floor((distance % (1000*60*60)) / (1000*60));
-//   let seconds = Math.floor((distance % (1000 * 60)) / 1000);
-//
-//   document.querySelector(".days").innerText = days;
-//   document.querySelector(".hours").innerText = hours;
-//   document.querySelector(".minutes").innerText = minutes;
-//   document.querySelector(".seconds").innerText = seconds;
-//
-//   if(distance < 0){
-//     clearInterval(x)
-//     document.querySelector(".expired").innerText = "Expired";
-//   };
-//
-// }, 1000);
-//
-// $(".oo").corner("bite bl br 5px");
-// $(".ob").corner("bite tl tr 5px");
+const countDownDate = new Date("Nov 29, 2022 12:11:12").getTime();
+let x = setInterval(function () {
+  let now = new Date().getTime();
+  let distance = countDownDate - now;
+  let days = Math.floor(distance / (1000*60*60*24));
+  let hours = Math.floor((distance % (1000*60*60*24)) / (1000*60*60));
+  let minutes = Math.floor((distance % (1000*60*60)) / (1000*60));
+  let seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-const option ={
-    method: "GET",
-    mode: "no-cors"
-}
+  document.querySelector(".days").innerText = days;
+  document.querySelector(".hours").innerText = hours;
+  document.querySelector(".minutes").innerText = minutes;
+  document.querySelector(".seconds").innerText = seconds;
 
-fetch("http://localhost:3000/projects/amos", option)
-.then((response) => {
-    response.json()
-})
-.then((data) => {
-    console.log(data);
-});
+  if(distance < 0){
+    clearInterval(x)
+    document.querySelector(".expired").innerText = "Expired";
+  };
+
+}, 1000);
+
+$(".oo").corner("bite bl br 5px");
+$(".ob").corner("bite tl tr 5px");
